12a2993df88c2dabab7552a9757103ae8ce14e14
Grant <grant_d_lohman@aol.com>
2023-09-13 19:40:20 -0500

    added tests to cart discount as well as code in price discount

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -1,8 +1,7 @@
 def main():
-
     print(discount([10, 4, 20]))  # Expect this to print 4
     # what other lists might this function be called with?
-    
+
 
 def discount(item_prices):
     """ Complete this function that returns the discount earned for a list of item prices
@@ -10,7 +9,25 @@ def discount(item_prices):
     Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
 
-    pass  # todo replace this line with your code 
+    if not item_prices:
+        return 0
+
+    for price in item_prices:
+        if type(price) == str:
+            return 0
+
+    item_prices.sort()
+
+    lowest_price = item_prices[0]
+
+    if type(lowest_price) == int:
+        list_length = len(item_prices)
+        if list_length >= 3:
+            return lowest_price
+        else:
+            return 0
+    else:
+        return 0
 
 
 if __name__ == '__main__':
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -1,6 +1,7 @@
-import unittest 
+import unittest
 from unittest import TestCase
-from price_discount import discount  
+from price_discount import discount
+
 
 class TestDiscount(TestCase):
 
@@ -9,8 +10,45 @@ class TestDiscount(TestCase):
         expected_discount = 4
         self.assertEqual(expected_discount, discount(prices))
 
-    
-    # TODO more unit tests here. Each test should test one scenario
+    def test_list_of_empty_prices(self):
+        prices = []
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_ten_prices(self):
+        prices = [3, 10, 9, 1, 3, 2, 4, 5, 8, 7]
+        expected_discount = 1
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_two_prices(self):
+        prices = [10, 4]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_one_price(self):
+        prices = [10]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_one_string(self):
+        prices = ['a']
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_three_strings(self):
+        prices = ['a', 'b', 'c']
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_four_mixed_strings_number(self):
+        prices = ['a', '1', 'c', 4]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_four_random_Strings(self):
+        prices = ['?', '1', '\n', '$']
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
 
 
 if __name__ == '__main__':
